046e2c601cfcae8ae78425947f361c99005baa7f
Devin <devinmui@yahoo.com>
2016-04-10 11:58:53 -0700

    shit

--- element-bootstrap/app-jesse/files/js/element.js
+++ element-bootstrap/app-jesse/files/js/element.js
@@ -11,113 +11,83 @@
 	var format;
 	var MyElement = PlatformElement.extend({
 		initialize: function(){
+
 			url = this.settings.get("url")
 			method = this.settings.get("method")
-			params = this.settings.get("parameters").split(",")
+			params = this.settings.get("parameters")
 			headers = this.settings.get("headers")
 			external_edit = this.settings.get("external_edit")
 			format = this.settings.get("format").split(" ")
 
-			// if headers
-			console.log("data" + params)
-			console.log("headers" + headers)
-			if(headers && params){
-				$.ajaxSetup({headers: headers, data: params})
-			} else if(headers){
-				$.ajaxSetup({headers: headers})
-			} else if(data){
-				$.ajaxSetup({data: params})
+			if(url){
+				if(method === 'GET'){
+					console.log('GET')
+					jQuery.get(url).done(function(html){
+						console.dir(html)
+						var html_code=""
+						$('#api_data').empty()
+						if(html.length === undefined){
+							for (i=0; i<format.length; i++){
+									if(format[i] === "<br>"){
+										html_code += "<br>"
+									} else {
+										var item = html[format[i]] // html[0]["name"]
+										html_code += item + " " // return something like "John"
+									}
+							}
+							$('#api_data').append("<p>" + html_code + "</p>") // "John j@j.com <br> 16"
+						} else {
+							for(x=0;x<html.length;x++){
+								for (i=0; i<format.length; i++){
+									if(format[i] === "<br>"){
+										html_code += "<br>"
+									} else {
+										var item = html[x][format[i]] // html[0]["name"]
+										console.log(item)
+										html_code += item + " "// return something like "John"
+									}
+								}
+								$('#api_data').append("<p>" + html_code + "</p>") // "John j@j.com <br> 16"
+							}
+						}
+					}).fail(function(error){
+						console.log(error)
+					})
+				} else if(method === 'POST'){
+					jQuery.post(url, params).done(function(html){
+						console.dir(html)
+						var html_code=""
+						$('#api_data').empty()
+						if(html.length === undefined){
+							for (i=0; i<format.length; i++){
+									if(format[i] === "<br>"){
+										html_code += "<br>"
+									} else {
+										var item = html[format[i]] // html[0]["name"]
+										html_code += item + " " // return something like "John"
+									}
+							}
+							$('#api_data').append("<p>" + html_code + "</p>") // "John j@j.com <br> 16"
+						} else {
+							for(x=0;x<html.length;x++){
+								for (i=0; i<format.length; i++){
+									if(format[i] === "<br>"){
+										html_code += "<br>"
+									} else {
+										var item = html[x][format[i]] // html[0]["name"]
+										console.log(item)
+										html_code += item + " "// return something like "John"
+									}
+								}
+								$('#api_data').append("<p>" + html_code + "</p>") // "John j@j.com <br> 16"
+							}
+						}
+					})
+				}
 			}
 
-			var item = {
-				url: url,
-				/*data: params,
-				headers: headers*/
-			}
-
-			if(method === 'GET'){
-				jQuery.get(url).done(function(html){
-					console.dir(html)
-					jsonParser(html,format)
-				}).fail(function(error){
-					console.log(error)
-				})
-			} else if(method === 'POST'){
-				$.post(url, params).done(function(html){
-					jsonParser(html,format)
-				})
-			} else if(method === 'PUT'){
-				$.put(item).done(function(html){
-					jsonParser(html,format)
-				})
-			} else if(method === 'DELETE'){
-				$.delete(item).done(function(html){
-					jsonParser(html,format)
-				})
-			} else if(method === 'PATCH'){
-				$.patch(item).done(function(html){
-					jsonParser(html,format)
-				})
-			}
 		}
 	});
 
 	return MyElement;
 })();
-
-// define custom methods
-jQuery.each( [ "put", "delete", "patch" ], function( i, method ) {
-  jQuery[ method ] = function( url, data, callback, type ) {
-    if ( jQuery.isFunction( data ) ) {
-      type = type || callback;
-      callback = data;
-      data = undefined;
-    }
-
-    return jQuery.ajax({
-      url: url,
-      type: method,
-      dataType: type,
-      data: data,
-      success: callback
-    });
-  };
-});
-
-function jsonParser(html, format){
-	console.log(format)
-	if(html.length === undefined){
-		for (i=0; i<format.length; i++){
-				if(format[i] === "<br>"){
-					html_code += "<br>"
-				} else if(format[i] === "\s"){ // cuz spaces stripped out
-					html_code += " "
-				} else {
-					var item = html[format[i]] // html[0]["name"]
-					html_code += item // return something like "John"
-				}
-		}
-		html_code = html_code.replace("undefined", "")
-		$('#api_data').append("<p>" + html_code + "</p>") // "John j@j.com <br> 16"
-		//$('#results').append("<p>" + html_code.replace("undefined", "") + "</p>") // i feel sorry for people named undefined now
-		//unshrekt ex de nice troll plox
-	} else {
-		for(x=0;x<html.length;x++){
-			var html_code=""
-			for (i=0; i<format.length; i++){
-				if(format[i] === "<br>"){
-					html_code += "<br>"
-				} else if(format[i] === "\s"){ // cuz spaces stripped out
-					html_code += " "
-				} else {
-					var item = html[x][format[i]] // html[0]["name"]
-					console.log(item)
-					html_code += item // return something like "John"
-				}
-			}
-		}
-		html_code = html_code.replace("undefined", "")
-		$('#api_data').append("<p>" + html_code + "</p>") // "John j@j.com <br> 16"
-		//$('#results').append("<p>" + html_code + "</p>")
-	}
-}
